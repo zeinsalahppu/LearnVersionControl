e479e1f5f1ebee81c4bf2c690b05ec002d2b52e0
zeinsalahppu <zeinsalah@gmail.com>
2020-11-09 10:02:37 +0200

    defined three functions

--- LearnVersionControl/LearnVersionControl.cpp
+++ LearnVersionControl/LearnVersionControl.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 class SinglyLinkedList
 {
-public:
-  SinglyLinkedList();
-  ~SinglyLinkedList();
+  public:
+    SinglyLinkedList();
+    ~SinglyLinkedList();
 
-private:
-  struct Node
-  {
-    int  value;
-    Node *next;
-  };
+    void addAtBeginning(int n);
+    void addAtEnd(int n);
+    void insertIntoSortedList(int n);
+    void readAndCreate(bool sorted = false);
+    void printOut();
+    bool isSorted();
+    int getSize();
 
-  typedef Node *NodePTR;
+  private:
+    struct Node
+    {
+      int  value;
+      Node *next;
+    };
 
-  NodePTR m_Head;
+    typedef Node *NodePTR;
+
+    NodePTR m_Head;
 };
 
 
@@ -34,11 +42,62 @@ SinglyLinkedList::~SinglyLinkedList()
 }
 
 
+
+void SinglyLinkedList::addAtBeginning(int n)
+{
+  NodePTR x = new Node;
+  x->value = n;
+  x->next = m_Head;
+  m_Head = x;
+}
+
+
+void SinglyLinkedList::addAtEnd(int n)
+{
+  NodePTR x = new Node;
+  x->value = n;
+  x->next = NULL;
+
+  if (m_Head == NULL)
+    m_Head = x;
+  else
+  {
+    NodePTR t = m_Head;
+    while (t->next != NULL)
+      t = t->next;
+
+    t->next = x;
+  }
+}
+
+
+void SinglyLinkedList::printOut()
+{
+  NodePTR x = m_Head;
+  cout << "\n";
+  while (x != NULL)
+  {
+    cout << x->value << "  ";
+    x = x->next;
+  }
+}
+
+
 void main()
 {
   cout << "Hello! This is an example program for studying linked lists" << endl;
   SinglyLinkedList list;
 
+  list.addAtBeginning(5);
+  list.addAtBeginning(8);
+  list.addAtBeginning(12);
+
+  list.addAtEnd(15);
+  list.addAtEnd(16);
+  list.addAtEnd(17);
+  list.addAtEnd(18);
+
+  list.printOut();
 
   system("PAUSE");
 }
